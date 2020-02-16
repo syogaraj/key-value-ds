b0618405413116225d82c15a698734ed865a5f20
Yogaraj <yoga30696@gmail.com>
2020-02-16 09:47:41 +0530

    1.code formatted 2.mmap resize to configured max space 3.added method to delete all keys at once. 4.flush method changed to fill empty space with bytes. 5.is_legit method modified to raise appropriate exceptions. 6.Added comment in DataStoreVO.

--- datastore/datastore.py
+++ datastore/datastore.py
@@ -1,41 +1,59 @@
-import threading, mmap, json, sys, time
+import json
+import mmap
+import sys
+import threading
+import time
+from collections import OrderedDict
 
 import config
 
 
-def is_legit(val, type="key"):
-    if type == "key":
+def is_legit(val, val_type="key"):
+    if val_type == "key":
+        if not isinstance(val, str):
+            raise ValueError(f"Key [{val}] must be of type str.")
         return len(val) <= config.MAX_KEY_LEN
-    elif type == "value":
+    elif val_type == "value":
         if isinstance(val, dict):
             return sys.getsizeof(val) <= config.MAX_VALUE_SIZE
-        return False
+        raise ValueError(f"Value [{val}] must be of type dict.")
 
 
 class DataStoreVO:
-    def __init__(self, value, ttl):
+    """
+        A simple ValueObject class which can be used to access value based on its ttl if provided.
+        If ttl is not provided, is_expired will always return False.
+    """
+
+    def __init__(self, value, created_at, ttl, *args, **kwargs):
         self.value = value
         self.ttl = ttl
-        self.created_at = int(time.time()*1000)
+        self.created_at = created_at
 
     def is_expired(self):
         if self.ttl is None:
             return False
-        curr_ts = int(time.time()*1000)
-        return (curr_ts - self.created_at) < self.ttl*1000
+        curr_ts = int(time.time() * 1000)
+        return (curr_ts - self.created_at) > self.ttl * 1000
 
 
 class DataStore:
     def __init__(self, file_descriptor, *args, **kwargs):
         self.__fd = file_descriptor
         self.__mmap = self._get_mmaped_fd()
-        self.__data = {}
-        self.__lock = threading.RLock()
+        self.__data = OrderedDict()
+        self.__lock = threading.Lock()
         self._read_data()
 
     def _get_mmaped_fd(self) -> mmap.mmap:
+        """
+            creates an mmap object for the provided file_descriptor. Always resizes the mmap size to config.MAX_LOCAL_STORAGE_SIZE
+        :return: mmap
+        """
         try:
-            return mmap.mmap(self.__fd, config.MAX_LOCAL_STORAGE_SIZE, access=mmap.ACCESS_WRITE)
+            mmaped_fd = mmap.mmap(self.__fd, 0, access=mmap.ACCESS_WRITE)
+            mmaped_fd.resize(config.MAX_LOCAL_STORAGE_SIZE)
+            return mmaped_fd
         except mmap.error:
             raise
 
@@ -47,20 +65,23 @@ class DataStore:
         with self.__lock:
             if key in self.__data:
                 raise ValueError(f"Key '{key}' already present.")
-            if is_legit(key, type="key") and is_legit(value, type="value"):
+            if is_legit(key, val_type="key") and is_legit(value, val_type="value"):
                 if ttl is not None:
                     try:
                         ttl = int(ttl)
                     except:
                         raise ValueError(f"Time-to-live {ttl} must be an integer value.")
-                value_tuple = DataStoreVO(value, ttl)
-                self.__data[key] = value_tuple
+                value_arr = [value, int(time.time() * 1000), ttl]
+                self.__data[key] = value_arr
                 self.flush()
+            else:
+                raise ValueError(
+                    f"Either provided key(allowed_size:{config.MAX_KEY_LEN} characters) or value(allowed_size:{config.MAX_VALUE_SIZE} bytes) doesn't meet the size config.")
 
     def delete(self, key) -> None:
         with self.__lock:
             if key not in self.__data:
-                raise ValueError(f"Key [{key}] not in datastore.")
+                return  # Ignore if key is non-existent
             del self.__data[key]
             self.flush()
 
@@ -68,18 +89,24 @@ class DataStore:
         with self.__lock:
             if key not in self.__data:
                 raise ValueError(f"Key [{key}] not in datastore.")
-            value = self.__data.get(key)  # type: DataStoreVO
+            value = DataStoreVO(*self.__data.get(key))  # type: DataStoreVO
             if value.is_expired():
-                del self.__data[key]
+                self.__data.pop(key)
                 self.flush()
                 raise ValueError(f"Key [{key}] Time-to-live expired.")
             return value.value
 
+    def delete_all(self):
+        with self.__lock:
+            self.__data = dict()
+            self.flush()
+
     def flush(self) -> None:
-        self.__mmap.write(bytes(str(self.__data).encode()))
+        self.__mmap.seek(0)
+        data_string = bytes(json.dumps(self.__data).encode('ascii'))
+        self.__mmap.write(data_string)
+        empty_space_bytes = self.__mmap.size() - self.__mmap.tell()
+        self.__mmap[self.__mmap.tell():] = b'\0' * empty_space_bytes
 
     def __getitem__(self, item):
         return self.get(item)
-
-    def __setitem__(self, key, value, ttl=None):
-        return self.create(key, value, ttl)
