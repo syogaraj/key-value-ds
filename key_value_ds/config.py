f6abd6358c6a7724c677732575089ca8e31f461c
Yogaraj <yoga30696@gmail.com>
2020-09-05 17:53:42 +0530

    renamed local storage to /tmp

--- key_value_ds/config.py
+++ key_value_ds/config.py
@@ -5,4 +5,4 @@ Configuration for defining the datastore.
 MAX_KEY_LEN = 32  # characters
 MAX_VALUE_SIZE = 16 * 1024  # 16 Kbytes
 MAX_LOCAL_STORAGE_SIZE = 1 * 1024 * 1024 * 1024  # 1 GB
-LOCAL_STORAGE_PREPEND_PATH = "/home/yogaraj"  # this will be prepended to the file path provided
+LOCAL_STORAGE_PREPEND_PATH = "/tmp"  # this will be prepended to the file path provided
